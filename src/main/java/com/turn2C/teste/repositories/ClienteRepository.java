08884c61881743f4f9ae57e24993acfafa86cb30
RenatoBorgess <118858842+RenatoBorgess@users.noreply.github.com>
2023-08-13 16:05:35 -0300

    Feat: creates crud operations endpoints.

--- src/main/java/com/turn2C/teste/repositories/ClienteRepository.java
+++ src/main/java/com/turn2C/teste/repositories/ClienteRepository.java
@@ -5,7 +5,13 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
+
 @Repository
 public interface ClienteRepository extends JpaRepository<Cliente, Long> {
+
+   Optional<Cliente> findByEmail(String email);
+
+
 
 }
